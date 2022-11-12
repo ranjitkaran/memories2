03f65a70a93fa0cc6729e783a084e919f0781da8
ranjitkaran <ranjitkaran360@gmail.com>
2022-11-12 23:32:14 +0530

    added done

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -16,9 +16,9 @@ const SignIn  = ()=>{
     const uploadPic = ()=>{
         const data = new FormData()
         data.append("file",image)
-        data.append("upload_preset","new-insta")
-        data.append("cloud_name","cnq")
-        fetch("https://api.cloudinary.com/v1_1/cnq/image/upload",{
+        data.append("upload_preset","insta-clone")
+        data.append("cloud_name","ranjitkaran")
+        fetch("https://api.cloudinary.com/v1_1/ranjitkaran/image/upload",{
             method:"post",
             body:data
         })
@@ -71,7 +71,7 @@ const SignIn  = ()=>{
    return (
       <div className="mycard">
           <div className="card auth-card input-field">
-            <h2>Instagram</h2>
+            <h2>memories</h2>
             <input
             type="text"
             placeholder="name"
